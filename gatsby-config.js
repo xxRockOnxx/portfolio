820464a438e51b42084ce535f945bdaacefbc841
Lemuel Flores <is101.lemuel@gmail.com>
2022-06-01 17:00:46 +0800

    remove gatsby-plugin-manifest and add gatsby-plugin-canonical-urls

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,15 +7,10 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: `gatsby-plugin-canonical-urls`,
       options: {
-        name: `gatsby-starter-tailwind`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#4dc0b5`,
-        display: `minimal-ui`,
-        icon: `src/images/tailwind-icon.png`,
+        // https://docs.netlify.com/configure-builds/environment-variables/#deploy-urls-and-metadata
+        siteUrl: process.env.URL,
       },
     },
     `gatsby-plugin-postcss`,
