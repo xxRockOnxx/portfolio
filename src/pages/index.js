72824c955d1c5f92c9dcd764539bde74e762570a
Lemuel <is101.lemuel@gmail.com>
2022-06-04 12:29:22 +0800

    new home page description

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,26 +8,39 @@ function IndexPage() {
     <Layout>
       <SEO title="Home" />
 
-      <div className="text-center">
+      <div>
         <img
           src={author}
-          className="border-grey-lighter mx-auto block rounded-full border-4 border-solid"
+          className="border-grey-lighter mb-8 block rounded-full border-4 border-solid"
           height={160}
           width={160}
           alt="Author"
         />
 
-        <h1 className="my-8 inline-block text-3xl font-bold text-gray-800">
+        <h1 className="mb-8 inline-block text-3xl font-bold text-gray-800">
           Hey there, I'm Lemuel Flores
         </h1>
 
-        <p className="mb-4 text-2xl">
-          I love quality codes and I am constantly learning new kinds of stuff.
+        <p className="mb-6 text-xl sm:text-2xl">
+          Constantly learning different language, libraries, and frameworks.
         </p>
-        <p className="text-2xl">
-          I can work with various kinds of projects. Learn more{" "}
-          <Link to="/about" className="font-medium underline">
+
+        <p className="mb-6 text-xl sm:text-2xl">
+          Building framework-agnostic skills.
+        </p>
+
+        <p className="text-xl sm:text-2xl">
+          <span>Learn more </span>
+          <Link to="/about/" className="font-medium underline">
             about me
+          </Link>
+          <span> and check out my </span>
+          <Link to="/projects/" className="font-medium underline">
+            projects
+          </Link>
+          <span> and </span>
+          <Link to="/blog/" className="font-medium underline">
+            blog
           </Link>
           .
         </p>
