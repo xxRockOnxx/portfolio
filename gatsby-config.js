34490454497bdc7633550330e7e0c23029240436
Lemuel <is101.lemuel@gmail.com>
2025-05-17 06:16:48 +0800

    chore: remove unused plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,59 +15,5 @@ module.exports = {
       },
     },
     `gatsby-plugin-postcss`,
-    {
-      resolve: "gatsby-plugin-fathom",
-      options: {
-        // Fathom server URL. Defaults to `cdn.usefathom.com`
-        trackingUrl: "lemuel-fathom.herokuapp.com",
-        // Unique site id
-        siteId: "REQMN",
-        // Domain whitelist
-        whitelistHostnames: ["lemuelflores.netlify.com"],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-disqus`,
-      options: {
-        shortname: `lemuelflores`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-prismjs`,
-            options: {
-              classPrefix: "language-",
-              inlineCodeMarker: null,
-              aliases: {},
-              showLineNumbers: false,
-              noInlineHighlight: false,
-              languageExtensions: [
-                {
-                  language: "superscript",
-                  extend: "javascript",
-                  definition: {
-                    superscript_types: /(SuperType)/,
-                  },
-                  insertBefore: {
-                    function: {
-                      superscript_keywords: /(superif|superelse)/,
-                    },
-                  },
-                },
-              ],
-              prompt: {
-                user: "root",
-                host: "localhost",
-                global: false,
-              },
-              escapeEntities: {},
-            },
-          },
-        ],
-      },
-    },
   ],
 };
