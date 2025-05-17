cfb25676a78404fca2d0d937c81fdf2c94749d47
Lemuel <is101.lemuel@gmail.com>
2025-05-17 09:11:10 +0800

    feat: migrate project to Astro

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: process.env.URL,
+  integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  redirects: {
+    "/resume":
+      "https://drive.google.com/file/d/1TMLDzlgmjahIMT7mA9XVVJUBcjvGzJXR/preview",
+  },
+});
